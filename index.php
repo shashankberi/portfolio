627522fdaaebbdfaf0d978f2b0e21e448ad82762
shashankberi <sberi243@gmail.com>
2020-04-15 20:26:47 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="./Bootstrap_4.3.1/css/bootstrap.css" rel="stylesheet" type="text/css" crossorigin="anonymous"/>
+  </head>
+
+  <body>
+    <div class= "container p-0">
+      <div class="nav-pills">
+        <ul class="nav nav-pills nav-fill">
+          <li class= "nav-item">
+            <a class= "nav-link active" href= "index.html"> Home</a>
+          </li>
+          <li class= "nav-item">
+            <a class="nav-link" href= "Beri_AboutMe.html">About Me</a>
+          </li>
+          <li class= "nav-item">
+            <a class="nav-link" href= "Beri_Resume.html">Resume</a>
+          </li>
+          <li class= "nav-item">
+            <a class="nav-link" href= "Beri_Projects.html">Projects</a>
+          </li>
+          <li class= "nav-item">
+            <a class="nav-link" href= "Beri_ContactMe.html">Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+<!--Jumbotron Header-->
+    <div class= "jumbotron">
+      <br>
+      <h1 align=center>Shashank Beri's Engineering Portfolio</h1>
+      <p align=center>My name is Shashank Beri and I attend Dublin High School, Dublin, CA. Explore around my website to learn more about me and see all the work I have done throughout my high school career.</p>
+    </div>
+  </body>
+  <section class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card flex-md-row">
+            <div class="card-body">
+              <strong>About Me</strong>
+              <h3>Hello!</h3>
+              <p>My name is Shashank Beri. I am a current junior at Dublin High School.</p>
+              <a href="Beri_AboutMe.html">Continue Reading</a>
+            </div>
+            <img class="card-img-right" style="max-width:250px; max-height:250px" src="Images/Shashank_MC.jpeg" alt="Card image">
+          </div>
+        </div>
+<!--The next box is below-->
+        <div class="col-md-6">
+          <div class="card flex-md-row">
+            <div class="card-body">
+              <strong>Projects</strong>
+              <h3>Scratch Project</h3>
+              <p>We used Scratch to create a fun and interactive game!</p>
+              <a href="Beri_Projects.html">Continue Reading</a>
+            </div>
+            <img class="card-img-right" height="250px" width="300px" src="Images/Scratch_Project.png" alt="Card image">
+          </div>
+        </div>
+      </div>
+<!--Start of the next card row-->
+<!--The next box is below-->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card flex-md-row">
+            <div class="card-body">
+              <h3>Resume</h3>
+              <p>See what I have done throughout my high school tenure.</p>
+              <a href="Beri_Resume.html">Continue Reading</a>
+            </div>
+            <img class="card-img-right" width="410px" height="250px" src="Images/SBLogo.png" alt="Card image">
+          </div>
+        </div>
+<!--The next box is below -->
+        <div class="col-md-6">
+          <div class="card flex-md-row">
+            <div class="card-body">
+              <h3>Contact Me</h3>
+              <p>If you have any questions, feel free to reach out to me.</p>
+              <a href="Beri_ContactMe.html">Continue Reading</a>
+            </div>
+            <img class="card-img-right" style="max-width:250px; max-height:250px" src="Images/Mail.png" alt="Card image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>  
+  <footer>
+    <div class="jumbotron">
+      <div class="row p-4">
+        <div class="col-sm-2"><a href="Beri_ContactMe.html"><img src= "./Images/Mail.png" width="75"></a></div>
+        <div class="col-sm-4"><a href="https://www.instagram.com/s.beri2/"><img src= "./Images/instagram.png"  width="75"></a></div>
+        <div class="col"><a href="https://www.dublin.k12.ca.us/Domain/132"><img src="./Images/DHS2.png" width="75" align=right></a></div>
+      </div>
+    </div>
+  </footer>
+</html>
